d8b57819489a0fb823537357d9778e94d568f0d5
Shresth Kapoor <shresthkapoor7@gmail.com>
2025-06-06 02:35:47 -0400

    Initial commit

---
+++ Strands/Strands/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Strands
+//
+//  Created by Shresth Kapoor on 06/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Strands/Strands/StrandsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StrandsApp.swift
+//  Strands
+//
+//  Created by Shresth Kapoor on 06/06/25.
+//
+
+import SwiftUI
+
+@main
+struct StrandsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
